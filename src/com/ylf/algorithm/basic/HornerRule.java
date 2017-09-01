e524abd604857b3780adf6372ed586a7850a41d3
Jerry <ylf989@163.com>
2017-09-01 10:39:27 +0800

    add files

---
+++ src/com/ylf/algorithm/basic/HornerRule.java
@@ -0,0 +1,33 @@
+package com.ylf.algorithm.basic;
+
+/**
+ * 实现霍纳规则的多项式计算时间复杂度为O(N)
+ */
+public class HornerRule {
+
+	/**
+	 * Y=a0+a1*X+a2*X^2+.....+an*X^n
+	 *  =a0+X(a1+X(a2+X(a3+X(...+X(an-1+X*(an))))))
+	 * 
+	 * @param a 多项式的系数数组
+	 * @param n 系数数组的长度
+	 * @param x 输入
+	 * @return y 输出
+	 */
+	public static double hornerFunction(double[] a, int n, double X) {
+		double Y = 0;
+		int i = n - 1;
+		while (i >= 0) {
+			Y = a[i] + X * Y;
+			i--;
+		}
+		return Y;
+	}
+	
+	public static void main(String[] args) {
+		double data[] = new double[] { 1.0, 2, 2, 3, 4 };
+		double x = 3;
+		System.out.println(hornerFunction(data, data.length, x));
+	}
+
+}
---
+++ src/com/ylf/algorithm/sort/Sort.java
@@ -0,0 +1,103 @@
+package com.ylf.algorithm.sort;
+
+public class Sort {
+
+	/**
+	 * 冒泡排序
+	 */
+	public static void bubbleSort(int[] A) {
+		for (int i = 0; i < A.length; i++) {
+			for (int j = A.length - 1; j > i; j--) {
+				if (A[j - 1] > A[j]) {
+					int tmp = A[j - 1];
+					A[j - 1] = A[j];
+					A[j] = tmp;
+				}
+			}
+		}
+	}
+
+	/**
+	 * 插入排序，类似人工排序一副扑克牌
+	 */
+	public static void insertionSort(int[] A) {
+		for (int i = 1; i < A.length; i++) {
+			int pendingInsertItem = A[i];
+			int j = i - 1;
+			while (j >= 0 && A[j] > pendingInsertItem) {
+				A[j + 1] = A[j];
+				j--;
+			}
+			A[j + 1] = pendingInsertItem;
+		}
+	}
+
+	/**
+	 * 归并排序，分治法的一种
+	 */
+	public static void mergeSort(int[] A) {
+		mergeSort(A, 0, A.length - 1);
+	}
+
+	public static void mergeSort(int[] A, int p, int r) {
+		if (p < r) {
+			int q = (p + r) / 2;
+			mergeSort(A, p, q);
+			mergeSort(A, q + 1, r);
+			merge(A, p, q, r);
+		}
+	}
+
+	/**
+	 * 归并排序的辅助过程，用来合并两个已排序的序列
+	 * 
+	 * @param	A	待合并数组
+	 * @param	pqr	p<=q<r，且 A[p..q]和A[q+1..r]已排序
+	 */
+	private static void merge(int[] A, int p, int q, int r) {
+		int[] rangedA = new int[r - p + 1];
+		int currentLeft = p;
+		int currentRight = q + 1;
+		for (int i = 0; i < rangedA.length; i++) {
+			// 两个子序列都已用完
+			if (currentLeft > q && currentRight > r) {
+				break;
+			}
+			// 左边子序列已用完，右边没用完
+			if (currentLeft > q) {
+				rangedA[i] = A[currentRight];
+				currentRight++;
+				continue;
+			}
+			// 右边子序列已用完，左边没用完
+			if (currentRight > r) {
+				rangedA[i] = A[currentLeft];
+				currentLeft++;
+				continue;
+			}
+			// 两边都还没用完
+			if (A[currentLeft] > A[currentRight]) {
+				rangedA[i] = A[currentRight];
+				currentRight++;
+				continue;
+			} else {
+				rangedA[i] = A[currentLeft];
+				currentLeft++;
+				continue;
+			}
+		}
+		// 将排好序的数组复制回原数组
+		for (int i = 0; i < rangedA.length; i++) {
+			A[p + i] = rangedA[i];
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] items = new int[] { 5, 2, 1, 7, 3, 4, 8, 6 };
+		Sort.bubbleSort(items);
+		for (int i = 0; i < items.length; i++) {
+			System.out.print(items[i] + ((i == (items.length - 1)) ? "" : ", "));
+		}
+	}
+
+}
